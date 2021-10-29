b9b050122cabb896a4d4338c9e5ae7956a766d3b
Danni <sjaeledyr@gmail.com>
2021-10-29 13:29:15 -0400

    Add files via upload

---
+++ cogs/greetings/cog.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+
+
+class Greetings(commands.Cog, name='Greetings module'):
+
+    def __init__(self, bot):
+        self.bot = bot
+        print("WeebBot.greetings.cog.py has been loaded!")
+
+
+    @commands.command()
+    async def adhoc_play(self, ctx: commands.Context):
+        await ctx.send(f'O hai you go zai masu! {ctx.author.name}')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send(f'{member.mention} has joined Kowaii Weebo Trash!')
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send('You have entered an invalid command D:')
+
+
+def setup(bot):
+    bot.add_cog(Greetings(bot))
--- run.py
+++ run.py
@@ -1,18 +1,29 @@
 import discord
-from clients.custom_bot_client import TOKEN
-from clients.custom_bot_client import CustomBotClient
-from cogs.greetings import Greetings
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+class CustomBotClient(commands.Bot):
+
+    async def on_ready(self):
+        print(f'{self.user.name} has connected to Discord!')
 
 
 def main():
+    print("WeebBot is Loading!")
     intents = discord.Intents.default()
     intents.members = True
 
-    bot = CustomBotClient(
-        command_prefix='$',
-        intents=intents
-    )
-    bot.add_cog(Greetings(bot))
+    bot = CustomBotClient('$')
+
+# Load Cogs
+    for folder in os.listdir("cogs"):
+        if os.path.exists(os.path.join("cogs", folder, "cog.py")):
+            bot.load_extension(f"cogs.{folder}.cog")
 
     bot.run(TOKEN)
 
